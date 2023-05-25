709cb414e704b356431358ead2da726f033deb46
osw6858 <osw7875@naver.com>
2023-05-26 03:23:13 +0900

    'AddBanner'

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState, useRef } from 'react';
+import React, {useState, useRef} from 'react';
+import {Carousel} from 'antd';
 
 const App = () => {
     const [exercises, setExercises] = useState([]);
@@ -25,7 +26,7 @@ const App = () => {
                 }
             ]);
             setNewExercise('');
-        }else{
+        } else {
             alert('운동을 입력해 주세요.')
         }
     };
@@ -35,12 +36,17 @@ const App = () => {
         const weightInput = weightInputs.current[exerciseIndex];
         const repsInput = repsInputs.current[exerciseIndex];
 
+        if (parseInt(weightInput.value) > 800 || parseInt(repsInput.value) > 300) {
+            alert('잘못 입력한게 아닌지 확인해 주세요!');
+        }
+
         if (weightInput.value !== '' && repsInput.value !== '') {
             const updatedExercises = [...exercises];
 
-            updatedExercises[exerciseIndex].sets.push({weight: weightInput.value, repetitions: repsInput.value});
+            updatedExercises[exerciseIndex]
+                .sets
+                .push({weight: weightInput.value, repetitions: repsInput.value});
             setExercises(updatedExercises);
-
             weightInput.value = '';
             repsInput.value = '';
         } else {
@@ -51,6 +57,7 @@ const App = () => {
     /**세트를 제거하는 함수. 제거할 운동(exercises) index와 세트(sets) index를 매개변수로 받음   */
     const handleRemoveSet = (exerciseIndex, setIndex) => {
         const updatedExercises = [...exercises];
+        console.log(updatedExercises)
         updatedExercises[exerciseIndex]
             .sets
             .splice(setIndex, 1);
@@ -63,12 +70,42 @@ const App = () => {
     return (
         <div className="flex flex-col min-h-screen bg-slate-200">
             <header className="bg-gray-800 text-white py-4 px-8">
-                <h1 className="text-3xl font-bold">Workout Log</h1>
+                <h1 className="text-3xl py-2 font-bold">WorkOut__Log</h1>
                 <nav className="flex justify-center">
-                    <span className="text-gray-400 hover:text-white px-4">Home</span>
-                    <span className="text-gray-400 hover:text-white px-4">Exercise Log</span>
+                    <span className="text-gray-400 hover:text-white px-4 cursor-pointer">Home</span>
+                    <span className="text-gray-400 hover:text-white px-4 cursor-pointer">View Logs</span>
+                    <span className="text-gray-400 hover:text-white px-4 cursor-pointer">About</span>
                 </nav>
             </header>
+            <div className='w-4/6 p-8 mx-auto hidden sm:block'>
+                <Carousel >
+                    {/* PC 화면에서는 보임 */}
+                    <div>
+                        <h3
+                            className='bg-gray-100 h-40 rounded-lg text-2xl font-bold text-gray-400 flex flex-col justify-center items-center px-6'>Take Your Workout to the Next Level with Our App
+                            <span className='text-base font-normal mt-2'>Track your progress and stay motivated</span>
+                        </h3>
+                    </div>
+                    <div>
+                        <h3
+                            className='bg-gray-100 h-40 rounded-lg text-2xl font-bold text-gray-400 flex flex-col justify-center items-center px-6'>Get in Shape and Stay Accountable
+                            <span className='text-base font-normal mt-2'>Set your goals and track your results with ease</span>
+                        </h3>
+                    </div>
+                    <div>
+                        <h3
+                            className='bg-gray-100 h-40 rounded-lg text-2xl font-bold text-gray-400 flex flex-col justify-center items-center px-6'>Join a Community of Fitness Enthusiasts
+                            <span className='text-base font-normal mt-2'>Join challenges, share your workouts, and get inspired</span>
+                        </h3>
+                    </div>
+                    <div>
+                        <h3
+                            className='bg-gray-100 h-40 rounded-lg text-2xl font-bold text-gray-400 flex flex-col justify-center items-center px-6'>Thank you for Visite Our Page
+                            <span className='text-base font-normal mt-2'>Make every day count with our Page</span>
+                        </h3>
+                    </div>
+                </Carousel>
+            </div>
 
             <main className="flex-grow container mx-auto p-8">
                 <div className="grid grid-cols-2 gap-4 mb-8">
@@ -79,7 +116,7 @@ const App = () => {
                         value={newExercise}
                         onChange={handleInputChange}/>
                     <button
-                        className="bg-blue-500 text-white rounded-lg px-8 py-4"
+                        className="bg-blue-500 text-white rounded-lg px-8 py-4 hover:bg-sky-500"
                         onClick={handleAddExercise}>
                         Add Exercise
                     </button>
@@ -105,7 +142,7 @@ const App = () => {
                                     max={100}
                                     ref={(el) => repsInputs.current[exerciseIndex] = el}/>
                                 <button
-                                    className="bg-blue-500 text-white rounded-lg px-8 py-4"
+                                    className="bg-blue-500 text-white rounded-lg px-8 py-4 hover:bg-sky-500"
                                     onClick={() => handleAddSet(exerciseIndex)}>
                                     Add Set
                                 </button>
@@ -118,21 +155,24 @@ const App = () => {
                                         <div className="grid grid-cols-4 gap-4 mb-2 md:mb-4" key={setIndex}>
                                             {
                                                 set.weight !== '' && (
-                                                    <div className="bg-gray-300 rounded-lg px-2 md:px-8 py-2 md:py-4 text-center col-span-2 md:col-span-1">
+                                                    <div
+                                                        className="bg-gray-300 rounded-lg px-2 md:px-8 py-2 md:py-4 text-center font-sans col-span-2 md:col-span-1">
                                                         {`${setIndex} Set`}
                                                     </div>
                                                 )
                                             }
                                             {
                                                 set.weight !== '' && (
-                                                    <div className="bg-gray-300 rounded-lg px-2 md:px-8 py-2 md:py-4 text-center col-span-2 md:col-span-1">
+                                                    <div
+                                                        className="bg-gray-300 rounded-lg px-2 md:px-8 py-2 md:py-4 text-center font-sans col-span-2 md:col-span-1">
                                                         {`${set.weight}kg`}
                                                     </div>
                                                 )
                                             }
                                             {
                                                 set.repetitions !== '' && (
-                                                    <div className="bg-gray-300 rounded-lg px-2 md:px-8 py-2 md:py-4 text-center col-span-2 md:col-span-1">
+                                                    <div
+                                                        className="bg-gray-300 rounded-lg px-2 md:px-8 py-2 md:py-4 text-center font-sans col-span-2 md:col-span-1">
                                                         {`${set.repetitions} Reps`}
                                                     </div>
                                                 )
@@ -141,21 +181,24 @@ const App = () => {
                                             {
                                                 set.repetitions !== '' && (
                                                     <button
-                                                        className="bg-red-500 text-white rounded-lg px-2 md:px-8 py-2 md:py-4 text-center col-span-2 md:col-span-1"
+                                                        className="bg-red-500 hover:bg-red-400 text-white rounded-lg px-2 md:px-8 py-2 md:py-4 text-center col-span-2 md:col-span-1"
                                                         onClick={() => handleRemoveSet(exerciseIndex, setIndex)}>
                                                         Remove
                                                     </button>
                                                 )
                                             }
-                                            <hr class="w-fit mx-auto my-4"/>
+                                            <span className="opacity-0 text-center text-red-600 text-xs">견뎌</span>
                                         </div>
+
                                     ))
                             }
+
                         </div>
                     ))
                 }
 
-                <button className="bg-blue-500 text-white rounded-lg px-8 py-4">Complete</button>
+                <button
+                    className="bg-blue-500 text-white rounded-lg px-8 py-4  hover:bg-sky-500">Complete</button>
             </main>
 
             <footer className="bg-gray-800 text-white py-4 px-8">
